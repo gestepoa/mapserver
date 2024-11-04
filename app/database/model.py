0f55aae643de76f9a9e5c76b1a454cba3bcaf1df
gestepoa <gestepoA@gmail.com>
2024-11-05 00:08:20 +0800

    update .gitignore

--- app/database/model.py
+++ app/database/model.py
@@ -5,7 +5,10 @@ from app.database.config import Base
 class MapPoint(Base):
     __tablename__ = "map_points"
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(50), nullable=False)
-    latitude = Column(Float, nullable=False)
-    longitude = Column(Float, nullable=False)
+    id = Column(Integer, primary_key=True, comment="履历信息id")
+    spot_name = Column(String(255), nullable=False, unique=False, comment="履历信息id")
+    country_name = Column(String(255), nullable=True, comment="履历信息id")
+    note = Column(String(255), nullable=True, comment="履历信息id")
+    latitude = Column(Float, nullable=False, comment="履历信息id")
+    longitude = Column(Float, nullable=False, comment="履历信息id")
+    height = Column(Float, nullable=True, comment="履历信息id")
--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
 from app.api import map
-from app.database import model
-from app.database.config import engine
 
-model.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(map.router, prefix="/maps")

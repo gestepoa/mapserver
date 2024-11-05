21a2d2b5eaab185733252fbcd990445e078423f3
gestepoa <gestepoA@gmail.com>
2024-11-06 01:01:02 +0800

    `

--- app/database/model.py
+++ app/database/model.py
@@ -7,6 +7,9 @@ class MapPoint(Base):
 
     id = Column(Integer, primary_key=True, comment="id")
     spot_name = Column(String(255), nullable=False, unique=False, comment="点位名称")
+    spot_short_name = Column(String(255), nullable=False, unique=False, comment="点位简称")
+    spot_name_EN = Column(String(255), nullable=False, unique=False, comment="点位英文名称")
+    spot_short_name_EN = Column(String(255), nullable=False, unique=False, comment="点位英文简称")
     country_name = Column(String(255), nullable=True, comment="所属国家")
     note = Column(String(255), nullable=True, comment="备注")
     latitude = Column(Float, nullable=False, comment="纬度")

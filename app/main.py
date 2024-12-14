701fb2455e308a0b359d817cb794fe64e9131ee4
gestepoa <gestepoA@gmail.com>
2024-12-14 19:26:11 +0800

    `

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.api import map
+from app.api import fileTrans
 from app.database.config import close_all
 from contextlib import asynccontextmanager
 
@@ -13,6 +14,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(map.router, prefix="/maps")
+app.include_router(fileTrans.router, prefix="/files")
 
 @app.get("/")
 def read_root():
